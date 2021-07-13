4bd51a4ff47f5a148a60c8c01ea6329041ede1f5
nikitha26 <mnikitha365@gmail.com>
2021-07-13 20:22:39 +0530

    form validations

--- Form Validations/index.php
+++ Form Validations/index.php
@@ -1,15 +1,37 @@
-
-
-
 <!DOCTYPE HTML>
 <html>  
 <body>
-
-<form action="result.php" method="POST">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+<?php
+   $name = $email = $gender = "";
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $gender = test_input($_POST["gender"]);
+   }
+   function test_input($data){
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+   }
+?>
+<h3>Form Validation Basic</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+Name: <input type="text" name="name"><br><br>
+E-mail: <input type="text" name="email"><br><br>
+Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other<br><br>
 <input type="submit">
 </form>
+ <?php
+   echo $name ."</br>";
+   echo $email ."</br>";
+   echo $gender ."</br>";
+
+ ?>
 
 </body>
 </html>
--- Form Validations/result.php
+++ Form Validations/result.php
@@ -1,4 +1,5 @@
 <?php
+  
   echo 'Welcome '.$_POST["name"] .'</br>';
   echo 'Your Email Address is: '.$_POST["email"];
 ?>
